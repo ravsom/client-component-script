6885fcc621573615a6245536076738604f7bdead
ravi somayaji <ravikanth.somayaji@gmail.com>
2022-05-27 11:29:51 +0530

    first commit

---
+++ src/lib/components/Component.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let value: string;
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+	$: handleClick = () => dispatch('cmpBtnClick');
+</script>
+
+<div>Hello</div>
+<button on:click={handleClick}>Button in Component</button>
+
+<div>Value is here: {value}</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Component from '$lib/components/Component.svelte';
+
+	let c2: any;
+	const renderButton = () => {
+		const cmp = document.querySelector('#cmp');
+		c2 = new Component({
+			target: cmp as Element,
+		});
+		c2.$on('cmpBtnClick', () => {
+			counter++;
+		});
+	};
+
+	$: counter = 0;
+	$: {
+		c2?.$set({ value: counter });
+	}
+</script>
+
+<button on:click={renderButton}>Click</button>
+
+<div id="cmp">
+	<div id="hello2">Hello2</div>
+</div>
